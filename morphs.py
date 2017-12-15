40b178e9b0ee480d6ef9a71752213cdb758aab6a
bjohnson <bkj.322@gmail.com>
2017-12-14 22:20:26 -0500

    adding do_make_wider morphism

--- morphs.py
+++ morphs.py
@@ -63,13 +63,13 @@ class MorphMixin(object):
             return super(MorphMixin, self).forward(x)
         except:
             if self.allow_morph:
-                self._morph(x)
+                self.morph_in(x)
                 return super(MorphMixin, self).forward(x)
             else:
                 raise
 
 class MorphFlatLinear(MorphMixin, FlatLinear):
-    def _morph(self, x):
+    def morph_in(self, x):
         new_features = x.view(x.size(0), -1).size(-1)
         
         print('MorphFlatLinear: %d features -> %d features' % (self.in_features, new_features), file=sys.stderr)
@@ -81,20 +81,28 @@ class MorphFlatLinear(MorphMixin, FlatLinear):
 
 
 class MorphConv2d(MorphMixin, nn.Conv2d):
-    def _morph(self, x):
+    def morph_in(self, x):
         new_channels = x.size(1)
         
         print('MorphConv2d: %d channels -> %d channels' % (self.in_channels, new_channels), file=sys.stderr)
         
-        padding = torch.zeros((
-            self.out_channels,
-            new_channels - self.in_channels,
-            self.kernel_size[0],
-            self.kernel_size[1],
-        ))
+        padding = torch.zeros((self.out_channels, new_channels - self.in_channels) + self.kernel_size)
         
         self.in_channels = new_channels
         self.weight.data.set_(torch.cat([self.weight.data, padding], dim=1))
+    
+    def morph_out(self, new_channels):
+        
+        padding = torch.zeros((new_channels - self.out_channels, self.in_channels) + self.kernel_size)
+        
+        self.weight.data.set_(torch.cat([self.weight.data, padding], dim=0))
+        
+        if self.bias is not None:
+            padding = torch.zeros(new_channels - self.out_channels)
+            self.bias.data = torch.cat([self.bias.data, padding])
+        
+        self.out_channels = new_channels
+
 
 # --
 # Morphs
@@ -114,6 +122,7 @@ def do_add_skip(model, idx=None):
         raise Exception('dimensions do not agree!')
     
     return model
+
 
 def do_cat_skip(model, idx=None):
     if idx is None:
@@ -150,19 +159,41 @@ def do_make_deeper(model, idx=None):
     old_layer = model.graph[idx1][0]
     
     if isinstance(old_layer, nn.Conv2d):
-        if old_layer.in_channels != old_layer.out_channels:
-            raise Exception('cannot make idempotent version (layer shape)')
-            return model
-        
+        # Add idempotent Conv2d layer
         new_layer = copy.deepcopy(old_layer)
+        new_layer.in_channels = new_layer.out_channels
+        
         _ = new_layer.bias.data.zero_()
-        _ = new_layer.weight.data.zero_()
+        new_layer.weight.data = torch.zeros((new_layer.out_channels, new_layer.in_channels) + new_layer.kernel_size)
         new_layer.weight.data[:, :, 1, 1] = torch.eye(new_layer.in_channels).view(-1)
         model.modify_edge(idx1, idx2, new_layer)
+        
     else:
-        raise Exception('cannot make idempotent version (layer type)')
+        raise Exception('cannot make layer deeper')
     
     return model
+
+
+def do_make_wider(model, idx=None):
+    if idx is None:
+        idx = np.random.choice(model.top_layer, 1)[0]
+    
+    if isinstance(model.graph[idx][0], nn.Conv2d):
+        new_channels = 2 * model.graph[idx][0].out_channels
+        model.graph[idx][0].morph_out(new_channels=new_channels)
+        
+        for k, (layer, inputs) in model.graph.items():
+            try:
+                _ = model(X, k)
+            except:
+                layer.allow_morph = True
+                _ = model(X, k)
+                layer.allow_morph = False
+    else:
+        raise Exception('cannot make layer wider')
+    
+    return model
+
 
 # --
 # Setup
@@ -173,8 +204,8 @@ def make_model():
     set_seeds()
     return SeaNet({
         0 : (MorphConv2d(1, 32, kernel_size=3, padding=1), "data"),
-        1 : (MorphConv2d(32, 32, kernel_size=3, padding=1), 0),
-        2 : (MorphConv2d(32, 64, kernel_size=3, padding=1), 1),
+        1 : (MorphConv2d(32, 52, kernel_size=3, padding=1), 0),
+        2 : (MorphConv2d(52, 64, kernel_size=3, padding=1), 1),
         3 : (nn.MaxPool2d(2), 2),
         4 : (MorphFlatLinear(12544, 10), 3)
     })
@@ -215,7 +246,7 @@ opt.step()
 
 model = make_model()
 orig_scores = model(X)
-model = do_make_deeper(model, (1, 2))
+model = do_make_deeper(model, (0, 1))
 new_scores = model(X)
 
 assert np.allclose(to_numpy(orig_scores), to_numpy(new_scores))
@@ -224,5 +255,31 @@ opt = torch.optim.Adam(model.parameters(), lr=0.1)
 loss = F.cross_entropy(new_scores, y)
 loss.backward()
 opt.step()
+
+# --
+# Test widen layer
+
+model = make_model()
+orig_scores = model(X)
+model = do_make_wider(model, 0)
+new_scores = model(X)
+
+assert np.allclose(to_numpy(orig_scores), to_numpy(new_scores))
+
+opt = torch.optim.Adam(model.parameters(), lr=0.1)
+loss = F.cross_entropy(new_scores, y)
+loss.backward()
+opt.step()
+
+
+
+
+
+
+
+
+
+
+
 
 
