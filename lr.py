96122acd6b9051793ac1fd6a45955ace0c38c8bd
bjohnson <bkj.322@gmail.com>
2017-12-20 16:03:13 -0500

    testing

--- lr.py
+++ lr.py
@@ -28,39 +28,55 @@ class LRSchedule(object):
             param_group['lr'] = lr
     
     @staticmethod
-    def constant(x, lr_init=0.1, epochs=10):
-        return lr_init
-    
-    @staticmethod
-    def step(x, breaks=(150, 250)):
-        if x < breaks[0]:
-            return 0.1
-        elif x < breaks[1]:
-            return 0.01
-        else:
-            return 0.001
-    
-    @staticmethod
-    def linear(x, lr_init=0.1, epochs=10):
-        return lr_init * float(epochs - x) / epochs
-    
-    @staticmethod
-    def cyclical(x, lr_init=0.1, epochs=10):
-        """ Cyclical learning rate w/ annealing """
-        if x < 1:
-            # Start w/ small learning rate
-            return 0.05
-        else:
-            return lr_init * (1 - x % 1) * (epochs - np.floor(x)) / epochs
-    
-    @staticmethod
-    def sgdr(progress, period_length=50, lr_max=0.05, lr_min=0, t_mult=1):
-        if t_mult > 1:
-            period_id = np.floor(inv_power_sum(progress / period_length, t_mult)) + 1
-            offsets = power_sum(t_mult, period_id - 1) * period_length
-            period_progress = (progress - offsets) / (t_mult ** period_id * period_length)
+    def constant(lr_init=0.1, epochs=10):
+        def f(progress):
+            return lr_init
         
-        else:
-            period_progress = (progress % period_length) / period_length
+        return f
+    
+    @staticmethod
+    def step(breaks=(150, 250), **kwargs):
+        def f(progress):
+            if progress < breaks[0]:
+                return 0.1
+            elif progress < breaks[1]:
+                return 0.01
+            else:
+                return 0.001
         
-        return lr_min + 0.5 * (lr_max - lr_min) * (1 + np.cos(period_progress * np.pi))
+        return f
+    
+    @staticmethod
+    def linear(lr_init=0.1, epochs=10, **kwargs):
+        def f(progress):
+            return lr_init * float(epochs - progress) / epochs
+        
+        return f
+    
+    @staticmethod
+    def cyclical(lr_init=0.1, epochs=10, **kwargs):
+        def f(progress):
+            """ Cyclical learning rate w/ annealing """
+            if progress < 1:
+                # Start w/ small learning rate
+                return 0.05
+            else:
+                return lr_init * (1 - progress % 1) * (epochs - np.floor(progress)) / epochs
+        
+        return f
+    
+    @staticmethod
+    def sgdr(period_length=50, lr_init=0.05, lr_min=0, t_mult=1, **kwargs):
+        def f(progress):
+            """ Cosine learning rate annealing """
+            if t_mult > 1:
+                period_id = np.floor(inv_power_sum(progress / period_length, t_mult)) + 1
+                offsets = power_sum(t_mult, period_id - 1) * period_length
+                period_progress = (progress - offsets) / (t_mult ** period_id * period_length)
+            
+            else:
+                period_progress = (progress % period_length) / period_length
+            
+            return lr_min + 0.5 * (lr_init - lr_min) * (1 + np.cos(period_progress * np.pi))
+        
+        return f
---
+++ main.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+"""
+    main.py
+"""
+
+from __future__ import print_function, division
+
+import os
+import sys
+import json
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torchvision
+import torchvision.transforms as transforms
+
+from seanet import SeaNet
+import morph_ops as mo
+import morph_layers as mm
+
+from lr import LRSchedule
+from utils import progress_bar
+from helpers import set_seeds, to_numpy, colstring
+
+torch.set_default_tensor_type('torch.DoubleTensor')
+
+# --
+# Params
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=40)
+    parser.add_argument('--lr-schedule', type=str, default='cyclical')
+    parser.add_argument('--lr-init', type=float, default=0.1)
+    parser.add_argument('--no-cuda', action="store_true")
+    
+    return parser.parse_args()
+
+args = parse_args()
+
+# --
+# IO
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, 
+    batch_size=128, 
+    shuffle=True, 
+    num_workers=8,
+    pin_memory=True
+)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, 
+    batch_size=256, 
+    shuffle=False, 
+    num_workers=8,
+    pin_memory=True,
+)
+
+# --
+# Helpers
+
+def train(net, opt, lr_scheduler, epoch, trainloader):
+    _ = net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    
+    batches_per_epoch = len(trainloader)
+    
+    for batch_idx, (data, targets) in enumerate(trainloader):
+        if not args.no_cuda:
+            data, targets = data.double().cuda(), targets.cuda()
+        
+        data, targets = Variable(data), Variable(targets)
+        
+        # Set LR
+        LRSchedule.set_lr(opt, lr_scheduler(epoch + batch_idx / batches_per_epoch))
+        
+        opt.zero_grad()
+        outputs = net(data)
+        loss = F.cross_entropy(outputs, targets)
+        loss.backward()
+        opt.step()
+        
+        train_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+        
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+    return float(correct) / total
+
+
+def test(net, testloader):
+    _ = net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (data, targets) in enumerate(testloader):
+        if not args.no_cuda:
+            data, targets = data.double().cuda(), targets.cuda()
+        
+        data, targets = Variable(data, volatile=True), Variable(targets)
+        
+        outputs = net(data)
+        loss = F.cross_entropy(outputs, targets)
+        
+        test_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+        
+        progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+    return float(correct) / total
+
+
+# --
+# Define model
+
+set_seeds(123)
+net = SeaNet({
+    1 : (mm.MorphBCRLayer(3, 32, kernel_size=3, padding=1), 0),
+    2 : (nn.MaxPool2d(2), 1),
+    3 : (mm.MorphBCRLayer(32, 32, kernel_size=3, padding=1), 2),
+    4 : (nn.MaxPool2d(2), 3),
+    5 : (mm.MorphBCRLayer(32, 32, kernel_size=3, padding=1), 4),
+    6 : (mm.MorphFlatLinear(2048, 10), 5),
+}, input_shape=(3, 32, 32))
+
+if not args.no_cuda:
+    net = net.cuda()
+
+# --
+# Define optimizer
+
+lr_scheduler = getattr(LRSchedule, args.lr_schedule)(lr_init=args.lr_init, epochs=args.epochs)
+opt = torch.optim.SGD(net.parameters(), lr=lr_scheduler(0), momentum=0.9, weight_decay=5e-4)
+
+# --
+# Run
+
+train_accs, test_accs = [], []
+for epoch in range(0, args.epochs):
+    print("Epoch=%d" % epoch, file=sys.stderr)
+    
+    train_acc = train(net, opt, lr_scheduler, epoch, trainloader)
+    test_acc = test(net, testloader)
+    
+    train_accs.append(train_acc)
+    test_accs.append(test_acc)
+    print(json.dumps({'train_acc' : train_acc, 'test_acc' : test_acc}))
